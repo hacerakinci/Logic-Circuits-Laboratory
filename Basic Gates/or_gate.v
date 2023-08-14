5a3cd8ac5ef0c8cb1ce39dac5f1a8ab90571003c
Hacer Akıncı <108008177+hacerakinci@users.noreply.github.com>
2023-08-14 14:11:05 +0300

    Update or_gate.v

--- Basic Gates/or_gate.v
+++ Basic Gates/or_gate.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/23/2023 05:32:04 PM
-// Design Name: 
-// Module Name: or_gate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module or_gate(
     input  A,
