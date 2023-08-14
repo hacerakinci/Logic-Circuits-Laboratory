0620a8a74a49ed3bf0d556098abb32e3d960e8b6
Hacer Akıncı <108008177+hacerakinci@users.noreply.github.com>
2023-08-14 14:10:46 +0300

    Update nand_gate.v

--- Basic Gates/nand_gate.v
+++ Basic Gates/nand_gate.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/01/2023 12:40:03 PM
-// Design Name: 
-// Module Name: nand_gate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module nand_gate(
     input i_0,
