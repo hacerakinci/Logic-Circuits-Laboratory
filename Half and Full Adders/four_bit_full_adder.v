edecf757aadfd3be09a8ce87d315e7e7fd28a91f
Hacer Akıncı <108008177+hacerakinci@users.noreply.github.com>
2023-08-14 14:07:08 +0300

    Update four_bit_full_adder.v

--- Half and Full Adders/four_bit_full_adder.v
+++ Half and Full Adders/four_bit_full_adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/23/2023 05:50:50 PM
-// Design Name: 
-// Module Name: four_bit_full_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module four_bit_full_adder(
     input [3:0] i0,
