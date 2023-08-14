d27964154dd19780c162b426df5bb5ed40167c87
Hacer Akıncı <108008177+hacerakinci@users.noreply.github.com>
2023-08-14 14:07:31 +0300

    Update one_bit_half_adder.v

--- Half and Full Adders/one_bit_half_adder.v
+++ Half and Full Adders/one_bit_half_adder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/23/2023 05:03:57 PM
-// Design Name: 
-// Module Name: one_bit_half_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module one_bit_half_adder(
     input i0,
