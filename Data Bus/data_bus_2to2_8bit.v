6ad9ad1b67f8f9709babe2db58ce883c7c6fb776
Hacer Akıncı <108008177+hacerakinci@users.noreply.github.com>
2023-08-14 14:09:57 +0300

    Update data_bus_2to2_8bit.v

--- Data Bus/data_bus_2to2_8bit.v
+++ Data Bus/data_bus_2to2_8bit.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/28/2023 02:17:09 PM
-// Design Name: 
-// Module Name: data_bus_2to2_8bit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module data_bus_2to2_8bit(
     input wire [7:0] data1,
