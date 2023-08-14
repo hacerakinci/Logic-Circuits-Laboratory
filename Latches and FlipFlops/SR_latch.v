8307555036076cbca13eb3b7eaeebf3182bdac7b
Hacer Akıncı <108008177+hacerakinci@users.noreply.github.com>
2023-08-14 14:09:00 +0300

    Update SR_latch.v

--- Latches and FlipFlops/SR_latch.v
+++ Latches and FlipFlops/SR_latch.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/01/2023 03:43:13 PM
-// Design Name: 
-// Module Name: SR_latch
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module SR_latch(
     input [0:0] S,
